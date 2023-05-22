8f36df4a68ec760cc998a31abee9cdc35ece2822
Julio Becerra Gomez <95427281+juliobecerragomez@users.noreply.github.com>
2023-05-22 18:48:13 +0200

    VIDCS-924: Fix fps value in Background-Substractor-Video-Capturer linux sample (#36)

--- Background-Substractor-Video-Capturer/main.cpp
+++ Background-Substractor-Video-Capturer/main.cpp
@@ -274,7 +274,7 @@ int main(int argc, char** argv) {
   video_capturer->format = OTC_VIDEO_FRAME_FORMAT_YUV420P;
   video_capturer->width = static_cast<int>(opencv_video_capture.get(cv::CAP_PROP_FRAME_WIDTH));
   video_capturer->height = static_cast<int>(opencv_video_capture.get(cv::CAP_PROP_FRAME_HEIGHT));
-  video_capturer->fps = static_cast<int>(opencv_video_capture.get(cv::CAP_PROP_FRAME_WIDTH));
+  video_capturer->fps = static_cast<int>(opencv_video_capture.get(cv::CAP_PROP_FPS));
   opencv_video_capture.release();
 
   RendererManager renderer_manager;
